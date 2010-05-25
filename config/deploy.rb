6c1ad5d3113e0780fca9b72e0aabff1dc4dc2a3a
Skip Baney <twelvelabs@gmail.com>
2010-05-25 00:33:03 -0500

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,65 @@
+set :domain, "twelvelabs.com"
+set :user, "austinonrails"
+set :application, "austinonrails.twelvelabs.com"
+set :applicationdir, "/home/#{user}/#{application}"  # The standard Dreamhost setup
+
+set :repository, "git@github.com:twelvelabs/austinonrails.git"
+set :repository_cache, "git_cache"
+set :scm, :git
+set :ssh_options, { :forward_agent => true }
+
+set :use_sudo, false
+set :deploy_to, applicationdir
+set :deploy_via, :remote_cache
+
+default_run_options[:pty] = true
+
+set :chmod755, "app config db lib public vendor script script/* public/disp*"
+
+set :rails_env, "production"
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
+
+set :bundle_path, "/home/twelvelabs/.gems/bin/bundle"
+
+namespace :bundler do
+  task :create_symlink, :roles => :app, :except => { :no_release => true } do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run "mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}"
+  end
+  
+  task :bundle_new_release, :roles => :app, :except => { :no_release => true } do
+    bundler.create_symlink
+    run "cd #{release_path} && #{bundle_path} install --without test"
+  end
+  
+  task :lock, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_release} && #{bundle_path} lock;"
+  end
+  
+  task :unlock, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_release} && #{bundle_path} unlock;"
+  end
+end
+
+# HOOKS
+after "deploy:update_code" do
+  bundler.bundle_new_release
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '42902778ce999c2f618ae77424142c0d287409fec8a70c3659d877f64fd5fc89a31f652be3e93b5c715f0060807e8df325ee9928c24839c5fe904f95e354714c'
